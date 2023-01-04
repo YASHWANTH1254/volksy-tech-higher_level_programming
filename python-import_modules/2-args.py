970016035d11000cbe3e7d716a1f58a5942c0d61
root <yashwanthpbrightcomgroup.com>
2023-01-03 20:19:16 -0800

    2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv)
+    if length <= 1:
+        print("0 argument.")
+    else:
+        if length == 2:
+            print("{:d} argument:".format(length - 1))
+        else:
+            print("{:d} arguments:".format(length - 1))
+        for i in range(1, length):
+            print("{:d}: {}".format(i, sys.argv[i]))
