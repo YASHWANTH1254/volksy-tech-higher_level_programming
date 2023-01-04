619f592db22da90f5303d150a361f878776bce28
root <yashwanthpbrightcomgroup.com>
2023-01-04 01:19:48 -0800

    100

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+if _name_ == "_main_":
     import sys
-    args = len(sys.argv) - 1
-    if args != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    from calculator_1 import add, sub, mul, div
+    length = len(sys.argv)
+
+    if length < 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         sys.exit(1)
-        op = sys.argv[2]
-        if op != '+' and op != '-' and op != '*' and + op != '/':
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
-        from calculator_1 import add, sub, mul, div
+    else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
-        if op == '+':
-            print("{} + {} = {}".format(a, b, add(a, b)))
-        elif op == '-':
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif op == '*':
-            print("{} * {} = {}".format(a, b, mul(a, b)))
+        if sys.argv[2] == '+':
+            print('{} + {} = {}'.format(a, b, add(a, b)))
+        elif sys.argv[2] == '-':
+            print('{} - {} = {}'.format(a, b, sub(a, b)))
+        elif sys.argv[2] == "*":
+            print('{} * {} = {}'.format(a, b, mul(a, b)))
+        elif sys.argv[2] == '/':
+            print('{} / {} = {}'.format(a, b, div(a, b)))
         else:
-            print("{} / {} = {}".format(a, b, div(a, b)))
+            print('Unknown operator. Available operators: +, -, * and /')
+            sys.exit(1)
