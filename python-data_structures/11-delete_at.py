4cf07af5fe5d84fbff676870352990145292be01
root <yashwanthpbrightcomgroup.com>
2023-01-05 03:56:36 -0800

    11-delete_at.py

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     my_list = my_list.delete()
-    if idx = 0:
+    if idx < 0:
         return my_list
     elif idx >= len(my_list):
         return my_list
