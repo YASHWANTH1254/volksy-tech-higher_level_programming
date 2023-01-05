7529678d95700b6c5e4542d6e87d08dcc2e1b6ec
root <yashwanthpbrightcomgroup.com>
2023-01-05 03:22:12 -0800

    4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     my_list = my_list.copy()
     if idx < 0:
         return my_list
-    elif idx <= len(my_list):
+    elif idx >= len(my_list):
         return my_list
     else:
         my_list[idx] = element
