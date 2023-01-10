ef491381671fa88c1ee84a7c50109ba0a16c56c3
root <yashwanthpbrightcomgroup.com>
2023-01-10 01:40:33 -0800

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    return [list(map(lambda n: n**2, sublist)) for sublist in matrix]
+'''This is the "Square"  module'''
+class Square:
+    """A simple Square class"""
+    pass
