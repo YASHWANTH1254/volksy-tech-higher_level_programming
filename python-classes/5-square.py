4b8201fb3a25b378fb02b1fe2fb6b4b0fbbd319c
root <yashwanthpbrightcomgroup.com>
2023-01-11 04:45:19 -0800

    5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -6,11 +6,10 @@ class Square:
     """Represent a square."""
 
     def __init__(self, size):
-        """Initialize a new square."""
-
-            
-
-        
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
         self.size = size
 
     @property
