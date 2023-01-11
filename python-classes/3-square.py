30303d8362e2f6aa61d7e00d025b12e7ef1ab6fb
root <yashwanthpbrightcomgroup.com>
2023-01-10 20:59:39 -0800

    3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""class"""
+
+
+class Square:
+    """square size"""
+    def __init__(self,size=0):
+        """methods"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Area of squara"""
+        return (self.__size * self.__size)
