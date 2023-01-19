a87e6115e69f0007c3674ab82539c25a4d11d5e3
root <yashwanthpbrightcomgroup.com>
2023-01-18 19:57:58 -0800

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""the adding list"""
+"""file added."""
+from pathlib import Path
 import sys
+import json
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+args = []
+if Path('add_item.json').exists():
+    args = load_from_json_file('add_item.json')
+
+for i in range(1, len(sys.argv)):
+    args.append(str(sys.argv[i]))
+
+save_to_json_file(args, 'add_item.json')
