415014d2883e0728f2ad20573a47cf5e91a61614
root <yashwanthpbrightcomgroup.com>
2023-01-31 07:30:20 -0800

    7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,4 +1,9 @@
--- create database and table.
+-- Creates DB `hbtn_0d_usa`
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, state_id NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
+CREATE TABLE IF NOT EXISTS cities (
+       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       FOREIGN KEY (state_id) REFERENCES states(id)
+); 
