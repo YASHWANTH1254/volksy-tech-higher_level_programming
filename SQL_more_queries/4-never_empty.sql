bbe917f7aee5a656b9a63b8a3dc44b077994aec1
root <yashwanthpbrightcomgroup.com>
2023-01-31 04:02:32 -0800

    4-never_empty.sql

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,2 @@
 -- create new table id is the default value.
-CREATE TABLE IF NOT EXISTS 'id_not_null' (
-id INT DEFAULT 1,
-name VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));
