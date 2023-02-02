ebeb5b9197d9d07644e2097a23a1e516a5aa6638
root <yashwanthpbrightcomgroup.com>
2023-02-01 20:13:14 -0800

    10-genre_id_by_show.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,3 @@
 -- Lists all cities found in `hbtn_0d_usa`, and their states.
-SELECT cities.id, cities.name, states.name
-FROM cities
-LEFT JOIN states
-ON states.id = cities.state_id
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id
 ORDER BY cities.id;
