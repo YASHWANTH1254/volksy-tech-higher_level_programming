c8542e51f02526e5c0f959743ab67cb84288923c
root <yashwanthpbrightcomgroup.com>
2023-02-06 22:03:10 -0800

    0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,7 +3,7 @@
 import MySQLdb
 from sys import argv
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, 
+    db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     num_rows = cur.execute("SELECT * FROM states ORDER BY states.id")
