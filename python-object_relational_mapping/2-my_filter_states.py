e4a9b9f680ed42de7ef9f1f22f2203ded4f8a3ea
root <yashwanthpbrightcomgroup.com>
2023-02-07 01:15:14 -0800

    2-my_filter_states.py

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays all values in the states table."""
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    sql = "SELECT * FROM states WHERE name='{:s}'
+    ORDER BY states.id".format(argv[4])
+    cur.execute(sql)
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
+    cur.close()
+    db.close()
