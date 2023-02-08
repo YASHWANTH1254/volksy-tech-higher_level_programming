0d3ad89995fa96f07446ef21e4a57fadc800c6d0
root <yashwanthpbrightcomgroup.com>
2023-02-08 01:46:15 -0800

    5-filter_cities.py

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" the sql injection."""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT name from cities where state_id=3;")
+    for i in c:
+        print(i[0], i[1], i[2])
