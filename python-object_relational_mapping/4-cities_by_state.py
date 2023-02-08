7e5847b60c834d01a742ee395ef54cee9755cc49
root <yashwanthpbrightcomgroup.com>
2023-02-07 22:46:19 -0800

    4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT id, name FROM cities order by id;")
+    c.execute("SELECT id, name, state FROM cities order by id;")
     for i in c:
         if i[1] == sys.argv[4]:
             print(i)
