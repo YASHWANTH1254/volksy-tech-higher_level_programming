0946e31077c18a455d70f52aad1be0fcbc6afb7d
root <yashwanthpbrightcomgroup.com>
2023-02-08 21:38:14 -0800

    100-magic_string.py

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-
-def magic_string():
-    # function objects can have attributes, here at global count of calls
-    if hasattr(magic_string, 'calls'):
-        magic_string.calls += 1
-    else:
-        magic_string.calls = 1
-
-    return ', '.join(['Holberton'] * magic_string.calls)
+def magic_string(static={"count": 0}):
+    static["count"] += 1
+    return ', '.join(["Holberton" for i in range(static["count"])])
