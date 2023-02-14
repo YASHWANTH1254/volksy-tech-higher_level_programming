2df4f0f45adc8202c84e09fc37d3973a03ebb343
root <yashwanthpbrightcomgroup.com>
2023-02-13 21:02:13 -0800

    3-say_my_name.txt

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module built for Python 0x07 task 2. Error in project formatting scheme."""
+
+
+def say_my_name(first_name, last_name=""):
+    """Function that prints "My name is ", followed by the one or two string."""
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+    if type(last_name) is not str:
+        raise TypeError('last_name must be a string')
+    print("My name is " + first_name, end="")
+    if first_name is not "":
+        print(" ", end="")
+    print(last_name)
